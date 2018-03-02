ef3490ff43a1db5b59d43da26cd8d852a0928b39
Brent Calabresi <bcalabresi1@gmail.com>
2018-03-01 20:44:51 -0500

    initial

---
+++ pl/Part1.java
@@ -0,0 +1,27 @@
+package pl;
+
+import pl.core.KB;
+import pl.core.Sentence;
+import pl.core.SymbolTable;
+
+/**
+ uses existing file WumpusWorldKB.java
+
+ */
+public class Part1 {
+
+    public static void main(String[] args) {
+
+    }
+
+    boolean enumerateTruthTable(KB kb, Sentence alpha){
+
+        if (kb.symbols().isEmpty()){
+            if ()
+        }
+
+        return false;
+    }
+
+
+}
